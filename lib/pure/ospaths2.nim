525dc689f2361e03c06dd8f9aecebadf0e8cd832
Timothee Cour <timothee.cour2@gmail.com>
2018-10-25 00:54:18 -0700

    _

--- lib/pure/ospaths2.nim
+++ lib/pure/ospaths2.nim
@@ -222,6 +222,7 @@ when true:
     else: result = base / RelativeFile file
 
 when isMainModule and defined(posix):
+  echo canon"/foo/../bar"
   doAssert canon"/foo/../bar" == "/bar"
   doAssert canon"foo/../bar" == "bar"
 
