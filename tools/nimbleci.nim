0d34b93299363bdcb17152f027cfaa5d1d3a9a92
Timothee Cour <timothee.cour2@gmail.com>
2019-01-10 04:15:22 -0800

    _

--- tools/nimbleci.nim
+++ tools/nimbleci.nim
@@ -2,7 +2,7 @@
 nimble wide CI
 ]#
 
-import std/[os, strutils, json, tables, macros, times]
+import std/[os, strutils, json, tables, macros, times, osproc, sequtils]
 import compiler/asciitables
 
 proc getNimblePkgPath(nimbleDir: string): string =
@@ -27,11 +27,13 @@ type Stats = object
   seenOK: int
   foundOK: int                ## whether nimble can still access it
   cloneOK: int
+  tasksOK: int
   installOK: int
   developOK: int
   buildOK: int
   testOK: int
   totalTime: float
+  tasks: seq[string]
 
 type TestResult = ref object
   # we can add further test fields here (eg running time, mem usage etc)
@@ -53,6 +55,14 @@ type Config = ref object
   pkgInstall: string
   pkgClone: string
 
+  ## what to run
+  runInstall: bool
+  runDevelop: bool
+  runClone: bool
+  runBuild: bool
+  runTasks: bool
+  runTest: bool
+
 proc runCIPackage(config: Config, data: var TestResult) =
   let t0 = epochTime()
   defer:
@@ -66,7 +76,7 @@ proc runCIPackage(config: Config, data: var TestResult) =
   data.stats.foundOK.inc
   let url = config.pkgs[pkg]["url"].getStr()
 
-  when true:
+  if config.runInstall:
     let cmd = "nimble install --nimbleDir:$# -y $# " % [config.pkgInstall,
         pkg]
     if execEcho(cmd):
@@ -76,19 +86,38 @@ proc runCIPackage(config: Config, data: var TestResult) =
   createDir config.pkgClone
   withDir config.pkgClone:
     if not existsDir pkg:
-      if execEcho("nimble develop --nimbleDir:$# -y $#" % [config.pkgInstall,
+      if config.runDevelop:
+        if execEcho("nimble develop --nimbleDir:$# -y $#" % [config.pkgInstall,
           pkg]):
-        data.stats.developOK.inc
-      if not execEcho("git clone $# $#" % [url, pkg]):
-        return                # nothing left to do without a clone
+          data.stats.developOK.inc
+      if config.runClone:
+        if not execEcho("git clone $# $#" % [url, pkg]):
+          return                # nothing left to do without a clone
     data.stats.cloneOK.inc
 
   withDir config.pkgClone / pkg:
-    data.stats.buildOK = ord execEcho("nimble build -N --nimbleDir:$#" % [
-        config.pkgInstall])
-    # note: see caveat https://github.com/nim-lang/nimble/issues/558
-    # where `nimble test` suceeds even if no tests are defined.
-    data.stats.testOK = ord execEcho "nimble test"
+    if config.runTasks:
+      # TODO :wrap w echo
+      let (outp, errC) = execCmdEx("nimble tasks")
+      if errC != 0:
+        echo "nimble tasks failed"
+      else:
+        data.stats.tasksOK.inc
+        for line in outp.splitLines:
+          var line = line
+          line = line.strip
+          # todo: pass `--hints:off --verbosity:0` once nimble supports it
+          if (line.startsWith "Warning:") or (line.startsWith "Hint:"):
+            continue
+          data.stats.tasks.add line.split[0]
+
+    if config.runBuild:
+      data.stats.buildOK = ord execEcho("nimble build -N --nimbleDir:$#" % [
+          config.pkgInstall])
+    if config.runTest:
+      # note: see caveat https://github.com/nim-lang/nimble/issues/558
+      # where `nimble test` suceeds even if no tests are defined.
+      data.stats.testOK = ord execEcho "nimble test"
 
 proc tabFormat[T](result: var string, a: T) =
   # TODO: more generic, flattens anything into 1 line of a table
@@ -98,7 +127,7 @@ proc tabFormat[T](result: var string, a: T) =
     else: result.add "\t"
     result.add k
     result.add ":\t"
-    result.add v
+    result.add $v
 
 proc `$`(a: TestResultAll): string =
   var s = ""
@@ -113,19 +142,22 @@ proc `$`(a: TestResultAll): string =
     s.add "\n"
   result = "TestResultAll:\n" & alignTable(s)
 
+proc updateField[T](a: var T, b: T) =
+  when T is SomeNumber:
+    a+=b
+  else:
+    # string etc
+    discard
+
 proc updateResults(a: var TestResultAll, b: TestResult) =
   a.tests.add b
   if b.stats.testOK == 0:
     a.failures.add b.pkg
   macro domixin(s: static[string]): untyped = parseStmt(s)
   for k, v in fieldPairs(b.stats):
-    domixin("a.stats.$# += b.stats.$#" % [k, k])
+    domixin("updateField(a.stats.$#, b.stats.$#)" % [k, k])
 
-proc runCIPackages*(dirOutput: string) =
-  echo "runCIPackages", (dirOutput: dirOutput, pid: getCurrentProcessId())
-    # pid useful to kill process, since because of a bug, ^C doesn't work inside exec
-
-  var data: TestResult
+proc getPkgs(): seq[string] =
   let pkgs0 = """
 # add more packages here; lines starting with `#` are skipped
 
@@ -153,29 +185,44 @@ iterutils
 gnuplot
 nimpb
 lazy
-choosenim
 """
 
+  for a in pkgs0.splitLines:
+    var a = a.strip
+    if a.len == 0: continue
+    if a.startsWith '#': continue
+    result.add a
+
+  echo (pkgs: result)
+
+proc runCIPackages*(dirOutput: string) =
+  echo "runCIPackages", (dirOutput: dirOutput, pid: getCurrentProcessId())
+    # pid useful to kill process, since because of a bug, ^C doesn't work inside exec
+
+  var data: TestResult
   var config = Config(
     pkgInstall: dirOutput/"nimbleRoot",
     pkgClone: dirOutput/"nimbleCloneRoot",
+    # runInstall: true,
+    # runDevelop: true,
+    runClone: true,
+    # runBuild: true,
+    runTasks: true,
+    # runTest: true,
   )
 
   doAssert execEcho "nimble refresh --nimbleDir:$#" % [config.pkgInstall]
   # doing it by hand until nimble exposes this API
   config.pkgs = config.pkgInstall.getNimblePkgPath.readFile.parseJson.parseNimble()
-
+  
+  # lets pkgs = getPkgs()
+  # let pkgs = config.pkgs.mapIt(it["name"].getStr())
+  # let pkgs = config.pkgs.mapIt(it["name"].getStr()) # todo: blocked by my Map PR
   var pkgs: seq[string]
-  for a in pkgs0.splitLines:
-    var a = a.strip
-    if a.len == 0: continue
-    if a.startsWith '#': continue
-    pkgs.add a
-
-  echo (pkgs: pkgs)
+  for k,v in config.pkgs:
+    pkgs.add k
 
   var testsAll: TestResultAll
-
   for i, pkg in pkgs:
     var data = TestResult(pkg: pkg)
     runCIPackage(config, data)
