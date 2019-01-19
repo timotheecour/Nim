d59366a0588078c8424aec15896f399166ccbf54
Timothee Cour <timothee.cour2@gmail.com>
2019-01-19 04:27:37 -0800

    _

--- ci_run.sh
+++ ci_run.sh
@@ -6,8 +6,8 @@ echo_run(){
   echo "$@" && "$@"
 }
 
-run_all(){
-  # doesn't seem to work wrt D20190119T040042
+install_nim_bootstarp(){
+  # this doesn't seem to work wrt D20190119T040042
   # export NIM_LIB_PREFIX=$(pwd)
 
   (
@@ -17,18 +17,23 @@ run_all(){
     echo_run env
     echo_run echo $MY_VAR
 
-    echo_run brew install nim
-    echo_run which nim
-    echo_run nim --version
+    if [[ "$OSTYPE" == "linux-gnu" ]]; then
+        # echo linux
+        echo_run apt-get install nim
+    elif [[ "$OSTYPE" == "darwin"* ]]; then
+      echo_run brew install nim
+      echo_run which nim
+      echo_run nim --version
 
-    # HACK, avoids: D20190119T040047:here
-    # koch.nim(27, 8) Error: cannot open file: /usr/local/Cellar/nim/0.19.0/nim/tools/ciutils
-    # maybe use 
-
-    echo_run cp $(which nim) bin/nim
+      # HACK, avoids:
+      # koch.nim(27, 8) Error: cannot open file: /usr/local/Cellar/nim/0.19.0/nim/tools/ciutils
+      # note: setting `NIM_LIB_PREFIX` doesn't work
+      echo_run cp $(which nim) bin/nim
+    else
+        echo unknown
+    fi
 
     # sh build_all.sh
-
     #git clone --depth 1 https://github.com/nim-lang/csources.git
     #cd csources
     #sh build.sh
@@ -36,5 +41,4 @@ run_all(){
   )
 }
 
-run_all
-
+install_nim_bootstarp
