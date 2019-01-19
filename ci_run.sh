3e344ffaa8f60b6d794de7db1ca01508787512b8
Timothee Cour <timothee.cour2@gmail.com>
2019-01-19 04:34:11 -0800

    _

--- ci_run.sh
+++ ci_run.sh
@@ -6,6 +6,17 @@ echo_run(){
   echo "$@" && "$@"
 }
 
+# https://stackoverflow.com/questions/3466166/how-to-check-if-running-in-cygwin-mac-or-linux
+unameOut="$(uname -s)"
+case "${unameOut}" in
+    Linux*)     machine=Linux;;
+    Darwin*)    machine=Mac;;
+    CYGWIN*)    machine=Cygwin;;
+    MINGW*)     machine=MinGw;;
+    *)          machine="UNKNOWN:${unameOut}"
+esac
+echo_run echo ${machine}
+
 install_nim_bootstarp(){
   # this doesn't seem to work wrt D20190119T040042
   # export NIM_LIB_PREFIX=$(pwd)
@@ -17,10 +28,11 @@ install_nim_bootstarp(){
     echo_run env
     echo_run echo $MY_VAR
 
-    if [[ "$OSTYPE" == "linux-gnu" ]]; then
-        # echo linux
+
+
+    if [[ "$machine" == "Linux" ]]; then
         echo_run apt-get install nim
-    elif [[ "$OSTYPE" == "darwin"* ]]; then
+    elif [[ "$OSTYPE" == "Mac" ]]; then
       echo_run brew install nim
       echo_run which nim
       echo_run nim --version
