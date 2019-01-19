137c20912f0819ca6eabb8ea43083ea3d9600041
Timothee Cour <timothee.cour2@gmail.com>
2019-01-19 00:50:02 -0800

    _

--- ci_run.sh
+++ ci_run.sh
@@ -1,6 +1,5 @@
 # for CI
 
-set -e
 set -u
 
 echo_run(){
@@ -9,16 +8,12 @@ echo_run(){
 
 run_all(){
   (
+    set -e
     echo $0
     echo $SHELL
     echo_run pwd
-    # echo pwd:$(pwd)
     # echo imageName:$(imageName)
-    # echo_run echo $(imageName)
-    # doesn't work: echo imageName2:${imageName}
-    # echo "env:"
     echo_run env
-
     echo_run echo $MY_VAR
 
     # note: node too old, gives SyntaxError: Unexpected token function
@@ -26,13 +21,16 @@ run_all(){
     # /usr/local/bin/node
     # Updating Homebrew...
 
+    echo_run npm install -g npm@next
+    # echo_run node --version
+    # echo_run which node
+    # # brew upgrade node
+    # echo_run brew install node || echo_run brew link --overwrite node
+    # echo_run which node
+
     echo_run node --version
-    echo_run which node
-    # brew upgrade node
-    echo_run brew install node || brew link --overwrite node
-    echo_run which node
-    echo_run node --version
-    echo_run echo "ok1"
+
+    echo_run echo "run_all ok"
   )
 }
 
