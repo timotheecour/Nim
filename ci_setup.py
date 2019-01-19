d3f55f2359dcb81dcb74e809c89c3304325669bd
Timothee Cour <timothee.cour2@gmail.com>
2019-01-19 05:28:42 -0800

    _

--- ci_setup.py
+++ ci_setup.py
@@ -4,7 +4,11 @@ import shutil
 def runCmd(cmd):
   print(("runCmd",cmd))
   import subprocess
-  subprocess.check_call(cmd, shell=True)
+  # subprocess.check_call(cmd, shell=True) # BUG: this doesn't print anything in azure!
+  output = subprocess.check_output(cmd, shell=True)
+  output=output.decode('ascii')
+  print("output:")
+  print(output)
   # subprocess.check_call(cmd, shell=True, stdout=subprocess.STDOUT)
 
 def setup():
@@ -17,16 +21,18 @@ def setup():
   elif platform == "darwin":
     # OS X
     # runCmd("pwd")
-    runCmd("echo foo1; sleep 10; echo foo2;sleep 10; echo foo3;")
-    # runCmd("brew install nim")
-    # runCmd("which nim")
-    # runCmd("nim --version")
-    # # runCmd("cp $(which nim) bin/nim") # IMPROVE
+    # runCmd("echo foo1; sleep 1; echo foo2;sleep 1; echo foo3;")
 
-    # old_nim = shutil.which('nim')
-    # print((old_nim,old_nim))
-    # assert(old_nim is not None)
-    # shutil.copyfile(old_nim, "bin/nim")
+    runCmd("brew install nim")
+    runCmd("which nim")
+    runCmd("nim --version")
+
+    # runCmd("cp $(which nim) bin/nim") # IMPROVE
+    old_nim = shutil.which('nim')
+    print((old_nim,old_nim))
+    assert(old_nim is not None)
+    shutil.copyfile(old_nim, "bin/nim")
+
   elif platform == "win32":
     # Windows...
     # TODO
