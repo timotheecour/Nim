4fa6a694adb1bd81cc1a03c030d8e5ad8bcd506e
Timothee Cour <timothee.cour2@gmail.com>
2019-02-11 13:15:24 -0800

    _

--- compiler/evalffi.nim
+++ compiler/evalffi.nim
@@ -10,7 +10,7 @@
 ## This file implements the FFI part of the evaluator for Nim code.
 
 import ast, astalgo, ropes, types, options, tables, dynlib, msgs, os
-import compiler/lineinfos
+import lineinfos
 import pkg/libffi
 
 when defined(windows):
