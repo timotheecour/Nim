534dba81a2168cb26255ee23eee01dcab75bace3
Timothee Cour <timothee.cour2@gmail.com>
2019-02-11 16:53:50 -0800

    _

--- compiler/evalffi.nim
+++ compiler/evalffi.nim
@@ -16,7 +16,8 @@ when defined(windows):
   const libcDll = "msvcrt.dll"
 elif defined(linux):
   # const libcDll = "libc.so(.6|.5|)"
-  const libcDll = "/usr/lib/x86_64-linux-gnu/libc.so(.6|.5|)"
+  # const libcDll = "/usr/lib/x86_64-linux-gnu/libc.so(.6|.5|)"
+  const libcDll = "/usr/lib/x86_64-linux-gnu/libc.so"
 elif defined(osx):
   const libcDll = "/usr/lib/libSystem.dylib"
 else:
