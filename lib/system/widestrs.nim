d3d01079492104bbb96f472f5fdb7081a4e732b4
Timothee Cour <timothee.cour2@gmail.com>
2019-04-16 11:48:28 -0700

    D20190412T164559

--- lib/system/widestrs.nim
+++ lib/system/widestrs.nim
@@ -140,6 +140,10 @@ iterator runes(s: cstring, L: int): int =
     fastRuneAt(s, i, L, result, true)
     yield result
 
+when defined(js):
+  # PRTEMP D20190412T164559
+  proc toU16(x: int): int16 {.magic: "ToU16", noSideEffect, deprecated.}
+
 proc newWideCString*(source: cstring, L: int): WideCStringObj =
   createWide(result, L * 4 + 2)
   #result = cast[wideCString](alloc(L * 4 + 2))
