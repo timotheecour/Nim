65eebc9f1319302f578c06484ec9acad3288cc02
Timothee Cour <timothee.cour2@gmail.com>
2020-03-09 19:05:00 -0700

    fixup

--- lib/system.nim
+++ lib/system.nim
@@ -2800,7 +2800,6 @@ when defined(himHasOverloadResolve):
     ## without calling it. Unlike `compiles(foo(args))`, the body is not analyzed.
     ## Also works with `compiles(mymod.mysym)` to return the symChoice overload
     ## set.
-    discard
 
 when defined(nimV2):
   import system/repr_v2
