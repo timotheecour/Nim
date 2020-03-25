ee8bfa8b8814cb3d3d1d9df1b98676112ceaeece
Timothee Cour <timothee.cour2@gmail.com>
2020-03-24 17:59:49 -0700

    _

--- tests/magics/mresolves.nim
+++ tests/magics/mresolves.nim
@@ -32,7 +32,7 @@ proc inspectImpl*(s: var string, a: NimNode, resolveLet: bool) =
       for i, ai in a20:
         a2.add if i notin [6]: ai else: newEmptyNode()
     s.add a2.lineInfoObj.toStr & " " & a2.repr
-  else: error($a.kind, a)
+  else: error($a.kind, a) # Error: nnkNilLit when couldn't resolve
 
 macro inspect*(a: typed, resolveLet: static bool = false): untyped =
   var a = a
@@ -44,3 +44,60 @@ macro inspect*(a: typed, resolveLet: static bool = false): untyped =
   inspectImpl(s, a, resolveLet)
   when defined(nimTestsResolvesDebug):
     echo s
+
+template inspect2*(a: untyped): untyped = inspect(resolveSymbol(a))
+
+macro fieldExistsImpl(a: typed): bool =
+  newLit(a.symKind == nskField)
+
+template fieldExists*(a: untyped): untyped = fieldExistsImpl(resolveSymbol(a))
+
+macro canImportImpl(a: typed): bool =
+  newLit(a.symKind == nskModule)
+
+# can't work like that...
+template canImport*(a: untyped): untyped = canImportImpl(resolveSymbol(a))
+
+macro inspect3*(a: typed): untyped =
+  echo (a.repr, a.kind, a.typeKind)
+  echo a.symKind
+  var a2 = a.getImpl
+  echo a2.kind
+  # echo a.sym.kind
+
+# macro getSymImpl(a: typed): NimSym =
+type SymWrap = object
+  s: NimSym
+type SymWrap2 = object
+  s: int
+type SymWrap3[T] = object
+  s: NimSym
+# macro getSymImpl(a: typed): NimSym =
+
+type SymWrap4*[sym] = object
+
+# type SymWrap4b*[sym] = object
+#   sym2: sym
+
+type SymWrap4b* = object
+  # sym2: T
+  sym2: NimSym
+
+macro getSymImpl(a: typed): untyped =
+  # NimSym
+  let x = a.symbol
+  result = quote do:
+    SymWrap4[`x`]
+
+template getSym*(a: untyped): untyped = getSymImpl(resolveSymbol(a))
+
+type SymInt* = distinct int
+macro getSymImpl2(a: typed): untyped =
+  # NimSym
+  let x = cast[int](a.symbol)
+  result = quote do:
+    # SymWrap4b[`x`](sym2: `x`)
+    # SymWrap4b(sym2: `x`)
+    `x`.SymInt
+
+template getSym2*(a: untyped): untyped = getSymImpl2(resolveSymbol(a))
