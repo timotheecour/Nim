6fa8f3a55a7e88b0d4c6ab6eeb6a1f06d5468893
Timothee Cour <timothee.cour2@gmail.com>
2020-05-29 17:38:05 -0700

    _

--- tools/kochdocs.nim
+++ tools/kochdocs.nim
@@ -236,8 +236,10 @@ proc buildDocPackages(nimArgs, destPath: string) =
   let extra = "-u:boot"
     # to avoid broken links to manual from compiler dir, but a multi-package
     # structure could be supported later
-  exec("$1 doc --project --outdir:$2/compiler $3 --git.url:$4 $5 compiler/nim.nim" %
-    [nim, destPath, nimArgs, gitUrl, extra])
+  exec("$1 -v" % [nim])
+  exec("$1 dump --dump.format:json " % [nim])
+  exec("$1 doc --project --outdir:$2/compiler $3 --git.url:$4 $5 $6" %
+    [nim, destPath, nimArgs, gitUrl, extra, "compiler" / "nim.nim"])
 
 proc buildDoc(nimArgs, destPath: string) =
   # call nim for the documentation:
@@ -314,7 +316,7 @@ proc buildDocs*(args: string) =
     let args2 = args
     createDir(dir2)
     buildDocSamples(args2, dir2)
-    buildDoc(args2, dir2)
+    # buildDoc(args2, dir2)
     buildDocPackages(args2, dir2)
     copyFile(docHackJsSource, dir2 / docHackJsSource.lastPathPart)
 
