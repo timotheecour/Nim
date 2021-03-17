96db819b45c235935ec94c2e9d773276799fe966
Timothee Cour <timothee.cour2@gmail.com>
2021-03-17 00:38:56 -0700

    _

--- ci/dockerrun.sh
+++ ci/dockerrun.sh
@@ -6,6 +6,6 @@ echo_run add-apt-repository -y ppa:git-core/ppa
 echo_run apt-get update
 echo_run apt-get install -yq cmake make gcc g++ git curl
 
-export CPU=i386
-echo_run sh build_all.sh
+# export CPU=i386
+echo_run sh build_all.sh --cpu i386
 echo_run ./koch runCI
