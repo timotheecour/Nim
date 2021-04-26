5337d5da79293729f554de6e59b7b47086da0c9a
Timothee Cour <timothee.cour2@gmail.com>
2021-04-26 15:11:46 -0700

    nim check: make error msgs less redundant with nkError

---
+++ tests/errmsgs/t16178_nimcheck_redundant.nim
@@ -0,0 +1,30 @@
+discard """
+cmd: '''nim check --hints:off $file'''
+action: reject
+# nimoutFull: true # pending https://github.com/nim-lang/Nim/pull/17865
+nimout: '''
+t16178_nimcheck_redundant.nim(22, 11) Error: undeclared identifier: 'bad5'
+t16178_nimcheck_redundant.nim(22, 7) Error: 'let' symbol requires an initialization
+t16178_nimcheck_redundant.nim(26, 7) Error: 'let' symbol requires an initialization
+'''
+"""
+
+
+
+
+#[
+xxx the line `Error: 'let' symbol requires an initialization` is redundant and should not
+be reported
+]#
+
+# line 20
+block:
+  let a = bad5(1)
+
+block: # bug #12741
+  macro foo = discard
+  let x = foo
+
+# block: # PRTEMP
+#   macro foo2 = discard
+#   discard foo2
