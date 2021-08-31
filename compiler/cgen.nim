06db5cc95d3593f85d4d0453abbd93c8e50e4f23
Timothee Cour <timothee.cour2@gmail.com>
2021-08-30 20:57:56 -0700

    cleanups

--- compiler/cgen.nim
+++ compiler/cgen.nim
@@ -1006,10 +1006,6 @@ proc isNoReturn(m: BModule; s: PSym): bool {.inline.} =
   sfNoReturn in s.flags and m.config.exc != excGoto
 
 proc genProcAux(m: BModule, prc: PSym) =
-  dbg prc.flags, prc
-  if sfForward in prc.flags:
-    # PRTEMP
-    return
   var p = newProc(prc, m)
   var header = genProcHeader(m, prc)
   var returnStmt: Rope = nil
@@ -2072,8 +2068,7 @@ proc genForwardedProcs(g: BModuleList) =
       prc = g.forwardedProcs.pop()
       m = g.modules[prc.itemId.module]
     if sfForward in prc.flags:
-      dbg "still forwarded: " & prc.name.s
-      # internalError(m.config, prc.info, "still forwarded: " & prc.name.s)
+      internalError(m.config, prc.info, "still forwarded: " & prc.name.s)
 
     genProcNoForward(m, prc)
 
