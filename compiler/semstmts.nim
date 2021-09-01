37789a09976555f98556bae361e415a44d123e55
Timothee Cour <timothee.cour2@gmail.com>
2021-08-31 21:38:16 -0700

    bugfix by avoiding returning lazyDecl in initOverloadIter, nextOverloadIter

--- compiler/semstmts.nim
+++ compiler/semstmts.nim
@@ -2020,8 +2020,8 @@ proc semProcAux(c: PContext, n: PNode, kind: TSymKind,
     if not hasProto:
       s.flags.excl sfForward
       s.flags.incl sfLazyForwardRequested
-    else:
-      s.flags.incl sfLazyImplmentation
+    elif proto != s:
+      s.lazyDecl = proto
 
   pragmaCallable(c, s, n, validPragmas)
   # PRTEMP after here, sfForward => sfImportc
