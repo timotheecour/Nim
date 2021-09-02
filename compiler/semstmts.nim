f74462bcee2fcc5c69fefd003662c48a1d20e237
Timothee Cour <timothee.cour2@gmail.com>
2021-09-02 10:25:41 -0700

    fix methods

--- compiler/semstmts.nim
+++ compiler/semstmts.nim
@@ -2180,6 +2180,7 @@ proc determineTypeOne(c: PContext, s: PSym) =
     case s.kind
     of skProc: procPragmas
     of skFunc: procPragmas
+    of skMethod: methodPragmas
     of skIterator: iteratorPragmas
     else: {} # PRTEMP
 
